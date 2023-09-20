0854734860ab227f10833bffcc6575f1dddd93a7
bishthub <1803013103@ipec.org.in>
2023-09-20 15:33:48 +0530

    cors added

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
-const jackpotRoutes = require('./routes/jackpotRoutes');
-const walletRoutes = require('./routes/walletRoutes');
-const blogRoutes = require('./routes/blogRoutes');
-const entryPassRoutes = require('./routes/entryPassRoutes');
-const transactionRoutes = require('./routes/transactionRoutes');
-const cartRoutes = require('./routes/cartRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const jackpotRoutes = require("./routes/jackpotRoutes");
+const walletRoutes = require("./routes/walletRoutes");
+const blogRoutes = require("./routes/blogRoutes");
+const entryPassRoutes = require("./routes/entryPassRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
 app.use(express.json());
-
-mongoose.connect('mongodb+srv://bisht:bishtji@dashboard.ihnggmw.mongodb.net/', {
+app.use(cors()); // Add this line
+mongoose.connect("mongodb+srv://bisht:bishtji@dashboard.ihnggmw.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-app.use('/api', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/product', productRoutes);
-app.use('/api/jackpot', jackpotRoutes);
-app.use('/api/wallet', walletRoutes);
-app.use('/api/blog', blogRoutes);
-app.use('/api/entryPass', entryPassRoutes);
-app.use('/api/transaction', transactionRoutes);
-app.use('/api/cart', cartRoutes);
+app.use("/api", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/jackpot", jackpotRoutes);
+app.use("/api/wallet", walletRoutes);
+app.use("/api/blog", blogRoutes);
+app.use("/api/entryPass", entryPassRoutes);
+app.use("/api/transaction", transactionRoutes);
+app.use("/api/cart", cartRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
