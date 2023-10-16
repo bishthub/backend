37b149bd63e9919b108108d13b66583314b0c0eb
anmolbisht2308 <anmolbisht7@gmail.com>
2023-10-16 15:09:54 +0530

    trial can-spin

--- controllers/jackpotController.js
+++ controllers/jackpotController.js
@@ -1,5 +1,5 @@
-const User = require('../models/userModel');
-const Wallet = require('../models/walletModel');
+const User = require("../models/userModel");
+const Wallet = require("../models/walletModel");
 
 function spinJackpot() {
   const randomNum = Math.floor(Math.random() * 100000) + 1;
@@ -49,7 +49,7 @@ exports.spin = async (req, res) => {
   res.send({ prize });
 };
 
-const items = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5', 'Item6', 'Item7'];
+const items = ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6", "Item7"];
 
 function spinItems() {
   const randomIndex = Math.floor(Math.random() * items.length);
@@ -94,8 +94,29 @@ exports.getWalletDetails = async (req, res) => {
   const wallet = await Wallet.findOne({ userId: req.user._id });
 
   if (!wallet) {
-    return res.status(404).send('Wallet not found');
+    return res.status(404).send("Wallet not found");
   }
 
   res.send(wallet);
+};
+
+exports.canSpin = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    if (!(await canSpin(userId))) {
+      return res.status(400).json({
+        canSpin: false,
+        message: "You've reached the maximum spins for the last 24 hours",
+      });
+    } else {
+      return res.status(200).json({
+        canSpin: true,
+        message: "You can Spin",
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- routes/jackpotRoutes.js
+++ routes/jackpotRoutes.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jackpotController = require('../controllers/jackpotController');
-const authMiddleware = require('../middleware/authMiddleware');
+const jackpotController = require("../controllers/jackpotController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Ensure the user is authenticated before accessing the jackpot routes
 router.use(authMiddleware);
 
-router.post('/spin', jackpotController.spin);
-router.get('/wallet/:id', jackpotController.getWalletDetails);
-router.post('/jackpotItemsSpinner', jackpotController.jackpotItemsSpinner);
+router.post("/spin", jackpotController.spin);
+router.get("/wallet/:id", jackpotController.getWalletDetails);
+router.post("/jackpotItemsSpinner", jackpotController.jackpotItemsSpinner);
+router.post("/can-spin", jackpotController.canSpin);
 
 module.exports = router;
