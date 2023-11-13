efe26cc74c2b19c1a78474c366221842033153ca
bishthub <1803013103@ipec.org.in>
2023-11-13 13:32:13 +0530

    prod

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use('/api/nftScan', nftScanRoutes);
 app.use('/api/nft', nftRoutes);
 app.use('/api/questions', questionRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
